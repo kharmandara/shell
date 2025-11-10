d6b08cefbb1d06d5985b7dfd58de1a7f4c1f5f05
Armita kh <kharmandara@users.noreply.github.com>
2025-11-10 02:19:54 -0500

    Check the grade

--- 02_activities/assignments/assignment.sh
+++ 02_activities/assignments/assignment.sh
@@ -28,12 +28,7 @@ unzip -q rawdata.zip
 # Complete assignment here
 
 # 1. Create a directory named data
-<<<<<<< HEAD
 mkdir data
-=======
-
-
->>>>>>> 51f608271341d2eb0f5a80b6f8a57c97a17d2cd5
 # 2. Move the ./rawdata directory to ./data/raw
 cd data
 mkdir raw
@@ -61,11 +56,10 @@ rm ../../processed/user_logs/*ipaddr*
 cd ../../processed
 ls
 touch ../inventory.txt
-#nano ../inventory.txt
 ls server_logs >> ../inventory.txt
 ls user_logs >> ../inventory.txt
 ls event_logs >> ../inventory.txt
 
 ###########################################
 
-echo "Project setup is complete!"'''
+echo "Project setup is complete!"
