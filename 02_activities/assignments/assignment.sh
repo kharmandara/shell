e3fbc6e810bf9635a095d24e959992dbedb30aa2
Armita kh <kharmandara@users.noreply.github.com>
2025-11-10 01:39:19 -0500

    assignment is done!

--- 02_activities/assignments/assignment.sh
+++ 02_activities/assignments/assignment.sh
@@ -28,22 +28,39 @@ unzip -q rawdata.zip
 # Complete assignment here
 
 # 1. Create a directory named data
-
+mkdir data
 # 2. Move the ./rawdata directory to ./data/raw
-
+cd data
+mkdir raw
+mv ../rawdata ./raw
 # 3. List the contents of the ./data/raw directory
-
+cd raw
+ls 
 # 4. In ./data/processed, create the following directories: server_logs, user_logs, and event_logs
-
+cd ../
+mkdir processed
+cd processed
+mkdir server_logs user_logs event_logs
+pwd
+ls
 # 5. Copy all server log files (files with "server" in the name AND a .log extension) from ./data/raw to ./data/processed/server_logs
-
+cd ../raw/rawdata
+cp *server*.log ../../processed/server_logs/
 # 6. Repeat the above step for user logs and event logs
-
+cp *user*.log ../../processed/user_logs/
+cp *event*.log ../../processed/event_logs/
 # 7. For user privacy, remove all files containing IP addresses (files with "ipaddr" in the filename) from ./data/raw and ./data/processed/user_logs
-
+rm *ipaddr*
+rm ../../processed/user_logs/*ipaddr*
 # 8. Create a file named ./data/inventory.txt that lists all the files in the subfolders of ./data/processed
-
+cd ../../processed
+ls
+touch ../inventory.txt
+#nano ../inventory.txt
+ls server_logs >> ../inventory.txt
+ls user_logs >> ../inventory.txt
+ls event_logs >> ../inventory.txt
 
 ###########################################
 
-echo "Project setup is complete!"
+echo "Project setup is complete!"'''
